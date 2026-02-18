77c2dbdb083230c278fcd418fa8648e7aca182d1
IsakEliH <isakelihauksson@gmail.com>
2026-02-18 19:36:26 +0000

    Queue is finished

--- queue.py
+++ queue.py
@@ -14,45 +14,41 @@ class Queue:
         """ "
         Constructor.
         """
-        ...
+        self._lst: SLList | DLList = lst
 
     def __len__(self):
         """ "
         Returns the number of elements in the queue.
         """
-        ...
-        return 0
+        return len(self._lst)
 
     def __str__(self):
         """
         Returns the string representation of the queue.
         """
-        ...
-        return ""
+        return str(self._lst)
 
     def is_empty(self):
         """
         Returns True if queue is empty, otherwise False.
         """
-        ...
-        return True
+        return self._lst.is_empty()
 
     def front(self):
         """
         Returns the front element of the queue (without removing it).
         :return: If non-empty, the front element of the queue, otherwise throws exception.
         """
-        ...
-        return None
+        return self._lst.front()
 
     def enqueue(self, item):
         """
         Inserts the element to the back of the queue.
         """
-        ...
+        self._lst.push_back(item)
 
     def dequeue(self):
         """
         Removes the element at the front of the queue (without returning)..
         """
-        ...
+        self._lst.pop_front()
